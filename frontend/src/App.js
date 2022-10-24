7bc5f23c9bab4bacd116e8ae80843b3d9768527b
E86742 <josh.leong@wnco.com>
2022-10-23 21:35:59 -0600

    added UI components and google fonts

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import axios from 'axios'
+import axios from 'axios';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import Button from '@mui/material/Button';
+
 
 function App() {
   const [getMessage, setGetMessage] = useState({})
 
   useEffect(() => {
-    axios.get('https://octopus-app-vtamg.ondigitalocean.app/flask/hello').then(response => {
+    axios.get('https://octopus-app-vtamg.ondigitalocean.app/').then(response => {
       console.log("SUCCESS", response)
       setGetMessage(response)
     }).catch(error => {
@@ -21,6 +27,7 @@ function App() {
         <h3>{getMessage.data.message}</h3>
         :
         <h3>LOADING</h3>}</div>
+        <Button variant="contained">Hello World</Button>
 
     </div>
   );
