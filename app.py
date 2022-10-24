0ca92893fbf8fa3a96eb4e239d9e71ef4ed626fd
E86742 <josh.leong@wnco.com>
2022-10-23 20:43:41 -0600

    removed db imports

--- app.py
+++ app.py
@@ -2,12 +2,12 @@ from flask import Flask, send_from_directory
 from flask_restful import Api, Resource, reqparse
 from flask_cors import CORS
 from api.HelloApiHandler import HelloApiHandler
-from flask_sqlalchemy import SQLAlchemy
+# from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_url_path='', static_folder='frontend/build')
 CORS(app)  # comment this on deployment
 api = Api(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
 
 
 @app.route("/", defaults={'path': ''})
@@ -17,43 +17,43 @@ def serve(path):
 
 api.add_resource(HelloApiHandler, '/flask/hello')
 # create the extension
-db = SQLAlchemy(app)
+# db = SQLAlchemy(app)
 # create the app
 # configure the SQLite database, relative to the app instance folder
 # initialize the app with the extension
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String, unique=True, nullable=False)
-    email = db.Column(db.String)
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String, unique=True, nullable=False)
+#     email = db.Column(db.String)
 
-def __init__(self, username, email):
-    self.username = username
-    self.email = email
+# def __init__(self, username, email):
+#     self.username = username
+#     self.email = email
 
-me = User(username='admin', email='admin@example.com')
-peter = User(username='peter', email='peter@example.com')
-guest = User(username='guest', email='guest@example.com')
+# me = User(username='admin', email='admin@example.com')
+# peter = User(username='peter', email='peter@example.com')
+# guest = User(username='guest', email='guest@example.com')
 
-with app.app_context():
-    db.init_app(app)
-    # db.create_all()
-    # db.session.add(me)
-    # User.query.all()
+# with app.app_context():
+#     db.init_app(app)
+#     # db.create_all()
+#     # db.session.add(me)
+#     # User.query.all()
 
-# users = User.query.all()
-# for user in users:
-#     print(user.username)
-#     print(f"<id={user.id}, username={user.username}")
+# # users = User.query.all()
+# # for user in users:
+# #     print(user.username)
+# #     print(f"<id={user.id}, username={user.username}")
 
-# db.session.close()
-@app.route('/')
-def index():
-    return 'Index Page'
+# # db.session.close()
+# @app.route('/')
+# def index():
+#     return 'Index Page'
 
-@app.route("/users/")
-def user_list():
-    users = User.query.all()
-    return render_template("users.html", users=users)
+# @app.route("/users/")
+# def user_list():
+#     users = User.query.all()
+#     return render_template("users.html", users=users)
 
 
