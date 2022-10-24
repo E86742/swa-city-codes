46eda2f87d2b957b103c51f264143f1dad4d1292
E86742 <josh.leong@wnco.com>
2022-10-23 21:17:07 -0600

    removed -r random file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios'
@@ -6,7 +5,7 @@ import axios from 'axios'
 function App() {
   const [getMessage, setGetMessage] = useState({})
 
-  useEffect(()=>{
+  useEffect(() => {
     axios.get('http://localhost:5000/flask/hello').then(response => {
       console.log("SUCCESS", response)
       setGetMessage(response)
@@ -17,14 +16,12 @@ function App() {
   }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>React + Flask Tutorial</p>
-        <div>{getMessage.status === 200 ? 
-          <h3>{getMessage.data.message}</h3>
-          :
-          <h3>LOADING</h3>}</div>
-      </header>
+
+      <div>{getMessage.status === 200 ?
+        <h3>{getMessage.data.message}</h3>
+        :
+        <h3>LOADING</h3>}</div>
+
     </div>
   );
 }
